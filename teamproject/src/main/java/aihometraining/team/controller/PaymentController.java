c6c9fbe7f97234a8dc28af1e6e15e92c4a20ea40
jinsugyeong <apr15th@naver.com>
2022-03-18 14:35:05 +0900

    결제 내역 controller 및 페이지 수정

--- teamproject/src/main/java/aihometraining/team/controller/PaymentController.java
+++ teamproject/src/main/java/aihometraining/team/controller/PaymentController.java
@@ -1,20 +1,20 @@
 package aihometraining.team.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class PaymentController {
 	
 	
-	private static final Logger log = LoggerFactory.getLogger(PaymentController.class);
-
-	
 	@GetMapping("/paymentList")
 	public String paymentList(Model model) {
+		
+		
 		
 		model.addAttribute("leftMenuList", "결제/환불");
 		model.addAttribute("title", "결제 내역");
