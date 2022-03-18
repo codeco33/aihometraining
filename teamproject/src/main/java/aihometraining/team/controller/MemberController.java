621b4aef13a29416080510670dea3454e4313e0e
Tim <developerTim@naver.com>
2022-03-18 16:29:28 +0900

    [team] 전체 회원 목록, 로그인 회원 목록 경로 설정
    DETAIL : 2022-03-18 LMS login 폴더 -> member 폴더
    MemberController 작업

---
+++ teamproject/src/main/java/aihometraining/team/controller/MemberController.java
@@ -0,0 +1,35 @@
+package aihometraining.team.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+	@GetMapping("/memberList")
+	public String memberList(Model model) {
+		
+		model.addAttribute("leftMenuList", "회원");
+		model.addAttribute("title", "전체 회원 목록 페이지");
+		
+		return "member/memberList";
+	}
+	@GetMapping("/memberLevelList")
+	public String memberLevelList(Model model) {
+		
+		model.addAttribute("leftMenuList", "회원");
+		model.addAttribute("title", "회원 권한 페이지");
+		
+		return "member/memberLevelList";
+	}
+	@GetMapping("/loginList")
+	public String loginList(Model model) {
+		
+		model.addAttribute("leftMenuList", "회원");
+		model.addAttribute("title", "로그인 회원 목록 페이지");
+		
+		return "member/loginList";
+	}
+}
