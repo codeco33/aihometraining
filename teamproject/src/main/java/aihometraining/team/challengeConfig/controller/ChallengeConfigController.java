6b4271ec4166f0a90137a008d87e015dd1a7a3ca
codeco33 <codeco33@naver.com>
2022-03-21 10:01:23 +0900

    [CBR] db값 가져오기 위해서 dto만듦, Member꺼도 만들었으니 나중에 수정해서 써주세요 mapper폴더 만들었습니다.

--- teamproject/src/main/java/aihometraining/team/challengeConfig/controller/ChallengeConfigController.java
+++ teamproject/src/main/java/aihometraining/team/challengeConfig/controller/ChallengeConfigController.java
@@ -1,19 +1,49 @@
 package aihometraining.team.challengeConfig.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import aihometraining.team.challengeConfig.service.ChallengeConfigService;
+import aihometraining.team.dto.ChallengeCategory;
+
 @Controller
 @RequestMapping("/challenge/challengeConfig")
 public class ChallengeConfigController {
 	
+	
+	private static final Logger log = LoggerFactory.getLogger(ChallengeConfigController.class);
+
+	
+	//DI 의존성 주입
+	private ChallengeConfigService challengeConfigService;
+	
+	public ChallengeConfigController(ChallengeConfigService challengeConfigService) {
+		this.challengeConfigService = challengeConfigService;
+	}
+	
 	@GetMapping("/configList")
 	public String challengeConfigList(Model model) {
 		
+		Map<String, Object> paramMap = new HashMap<String, Object>();
+		paramMap.put("memberEmail", "id001@email.com");
+		
+		List<ChallengeCategory> challengeCategoryList =  challengeConfigService.getChallengeCategoryList(paramMap);
+		
+		paramMap = null;
+		
+		log.info("챌린지 카테고리 조회  challengeCategoryList : {}", challengeCategoryList);
+		
 		model.addAttribute("title", "challengeConfigList");
 		model.addAttribute("leftMenuList", "챌린지 관리 설정");
+		model.addAttribute("challengeCategoryList", challengeCategoryList);
 		
 		return "challenge/challengeConfig/challengeConfigList";
 		
---
+++ teamproject/src/main/java/aihometraining/team/dto/ChallengeCategory.java
@@ -0,0 +1,80 @@
+package aihometraining.team.dto;
+
+public class ChallengeCategory {
+	/*
+	 *  challengeCategoryCode, 
+		memberEmail, 
+		challengeCategoryName, 
+		challengeCategoryRegDate, 
+		eClassCategorySmall, 
+		memberUpdateEmail,
+		memberUpdateDate
+	 */
+	private String challengeCategoryCode;
+	private String memberEmail;
+	private String challengeCategoryName;
+	private String challengeCategoryRegDate;
+	private String eClassCategorySmall;
+	private String memberUpdateEmail;
+	private String memberUpdateDate;
+	
+	private Member member;
+	
+	public String getChallengeCategoryCode() {
+		return challengeCategoryCode;
+	}
+	public void setChallengeCategoryCode(String challengeCategoryCode) {
+		this.challengeCategoryCode = challengeCategoryCode;
+	}
+	public String getMemberEmail() {
+		return memberEmail;
+	}
+	public void setMemberEmail(String memberEmail) {
+		this.memberEmail = memberEmail;
+	}
+	public String getChallengeCategoryName() {
+		return challengeCategoryName;
+	}
+	public void setChallengeCategoryName(String challengeCategoryName) {
+		this.challengeCategoryName = challengeCategoryName;
+	}
+	public String getChallengeCategoryRegDate() {
+		return challengeCategoryRegDate;
+	}
+	public void setChallengeCategoryRegDate(String challengeCategoryRegDate) {
+		this.challengeCategoryRegDate = challengeCategoryRegDate;
+	}
+	public String geteClassCategorySmall() {
+		return eClassCategorySmall;
+	}
+	public void seteClassCategorySmall(String eClassCategorySmall) {
+		this.eClassCategorySmall = eClassCategorySmall;
+	}
+	public String getMemberUpdateEmail() {
+		return memberUpdateEmail;
+	}
+	public void setMemberUpdateEmail(String memberUpdateEmail) {
+		this.memberUpdateEmail = memberUpdateEmail;
+	}
+	public String getMemberUpdateDate() {
+		return memberUpdateDate;
+	}
+	public void setMemberUpdateDate(String memberUpdateDate) {
+		this.memberUpdateDate = memberUpdateDate;
+	}
+	public Member getMember() {
+		return member;
+	}
+	public void setMember(Member member) {
+		this.member = member;
+	}
+	@Override
+	public String toString() {
+		return "challengeCategory [challengeCategoryCode=" + challengeCategoryCode + ", memberEmail=" + memberEmail
+				+ ", challengeCategoryName=" + challengeCategoryName + ", challengeCategoryRegDate="
+				+ challengeCategoryRegDate + ", eClassCategorySmall=" + eClassCategorySmall + ", memberUpdateEmail="
+				+ memberUpdateEmail + ", memberUpdateDate=" + memberUpdateDate + ", member=" + member + "]";
+	}
+	
+	
+}
