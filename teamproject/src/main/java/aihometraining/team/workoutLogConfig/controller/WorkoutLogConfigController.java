78da9e001f20d79c26a6dffe60b2843c1762b7dd
ddoggang12 <ddoggang12@naver.com>
2022-03-21 16:28:45 +0900

    [team] userfragments-header : 화면 연결

--- teamproject/src/main/java/aihometraining/team/workoutLogConfig/controller/WorkoutLogConfigController.java
+++ teamproject/src/main/java/aihometraining/team/workoutLogConfig/controller/WorkoutLogConfigController.java
@@ -3,6 +3,7 @@ package aihometraining.team.workoutLogConfig.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -22,11 +23,41 @@ public class WorkoutLogConfigController {
 	@GetMapping("/workoutCategoryList")
 	public String workoutCategoryList(Model model) {
 		
-		model.addAttribute("title", "운동 카테고리 목록");
+		model.addAttribute("title", "운동 계획 카테고리 목록");
 		model.addAttribute("leftMenuList", "일지");
 		
 		return "workoutLog/workoutLogConfig/workoutCategoryList";
 	}
+	
+	@PostMapping("/workoutCategoryInsert")
+	public String workoutCategoryInsert(Model model) {
+		
+		model.addAttribute("title", "운동 계획 카테고리 등록");
+		model.addAttribute("leftMenuList", "일지");
+		
+		return "workoutLog/workoutLogConfig/workoutCategoryInsert";
+	}
+	
+	@GetMapping("/workoutCategoryUpdate")
+	public String workoutCategoryUpdate(Model model) {
+		
+		model.addAttribute("title", "운동 계획 카테고리 수정");
+		model.addAttribute("leftMenuList", "일지");
+		
+		return "workoutLog/workoutLogConfig/workoutCategoryUpdate";
+		
+	}
+	
+	@GetMapping("/workoutCategoryDelete")
+	public String workoutCategoryDelete(Model model) {
+		
+		model.addAttribute("title", "운동 계획 카테고리 삭제");
+		model.addAttribute("leftMenuList", "일지");
+		
+		return "workoutLog/workoutLogConfig/workoutCategoryDelete";
+		
+	}
+	
 	
 	@GetMapping("/workoutAIVideoList")
 	public String workoutAIVideoList(Model model) {
@@ -37,5 +68,27 @@ public class WorkoutLogConfigController {
 		return "workoutLog/workoutLogConfig/workoutAIVideoList";
 		
 	}
+	
+	@GetMapping("/workoutAIVideoUpdate")
+	public String workoutAIVideoUpdate(Model model) {
+		
+		model.addAttribute("title", "AI 운동 영상 수정");
+		model.addAttribute("leftMenuList", "일지");
+		
+		return "workoutLog/workoutLogConfig/workoutAIVideoUpdate";
+		
+	}
+	
+	@GetMapping("/workoutAIVideoDelete")
+	public String workoutAIVideoDelete(Model model) {
+		
+		model.addAttribute("title", "AI 운동 영상 삭제");
+		model.addAttribute("leftMenuList", "일지");
+		
+		return "workoutLog/workoutLogConfig/workoutAIVideoDelete";
+		
+	}
+	
+	
 	
 }
