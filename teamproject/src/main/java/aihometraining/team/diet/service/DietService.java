5982d2becf5612a3a08eec0811be8bf8cd5b076b
pokou125 <94342364+pokou125@users.noreply.github.com>
2022-03-28 11:53:04 +0900

    [이영호] 식단은행 펭이지 / 컨트롤러 추가

---
+++ teamproject/src/main/java/aihometraining/team/diet/service/DietService.java
@@ -0,0 +1,15 @@
+package aihometraining.team.diet.service;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+@Transactional
+public class DietService {
+	
+	//DI 의존성 주입
+}
