10a6ec22c67700387348ef970aed105908ad38a1
codeco33 <codeco33@naver.com>
2022-03-31 23:15:49 +0900

    [CBR]참가하기버튼 눌렀을때 결제페이지로 연결, 값전달까지 완료

--- teamproject/src/main/java/aihometraining/team/challenge/controller/ChallengeEnterController.java
+++ teamproject/src/main/java/aihometraining/team/challenge/controller/ChallengeEnterController.java
@@ -1,13 +1,23 @@
 package aihometraining.team.challenge.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import aihometraining.team.dto.ChallengeGather;
 
 @Controller
 @RequestMapping("/challenge/challengeEnter")
 public class ChallengeEnterController {
+	
+	
+	private static final Logger log = LoggerFactory.getLogger(ChallengeEnterController.class);
+
+	
 	@GetMapping("/challengeEnterList")
 	public String challengeEnterList(Model model) {
 		
@@ -44,6 +54,15 @@ public class ChallengeEnterController {
 		model.addAttribute("headerList", "챌린지");
 		
 		return "challenge/challengeEnter/challengeEnterPaymemt";
+		
+	}
+	
+	@PostMapping("/challengeEnterPaymemt")
+	public String challengeEnterPaymemt(ChallengeGather challengeGather) {
+		
+		log.info("challengeGather 조회  challengeGather : {}", challengeGather);
+		
+		return null;
 		
 	}
 }
