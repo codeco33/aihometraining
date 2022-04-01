8635e2a603e19be9efec279e2fc0582ecc80d303
codeco33 <codeco33@naver.com>
2022-04-01 14:53:47 +0900

    [CBR]카테고리등록화면 수정

--- teamproject/src/main/java/aihometraining/team/challenge/controller/ChallengeConfigController.java
+++ teamproject/src/main/java/aihometraining/team/challenge/controller/ChallengeConfigController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import aihometraining.team.challenge.service.ChallengeConfigService;
 import aihometraining.team.dto.ChallengeCategory;
 import aihometraining.team.dto.EClassCategorySmall;
-import aihometraining.team.mapper.CommonMapper;
 
 @Controller
 @RequestMapping("/challenge/challengeConfig")
