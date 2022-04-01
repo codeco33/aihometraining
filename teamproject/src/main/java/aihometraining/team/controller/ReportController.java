62c607e79dc87f05a5865543bb3a5a4c31a7688e
Tim <developerTim@naver.com>
2022-04-01 11:21:57 +0900

    [LMS] 신고 기준 목록 조회 작업
    DETAIL : 220401 신고 기준 목록 조회 작업 DB연결 성공

--- teamproject/src/main/java/aihometraining/team/controller/ReportController.java
+++ teamproject/src/main/java/aihometraining/team/controller/ReportController.java
@@ -1,13 +1,30 @@
 package aihometraining.team.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import aihometraining.team.dto.ReportStandard;
+import aihometraining.team.mapper.ReportMapper;
+import aihometraining.team.service.ReportService;
+
+
 @Controller
 @RequestMapping("/report")
 public class ReportController {
+	
+	
+	//DI 의존성 주입 생성자 메소드 주입 방식
+	private ReportService reportService;
+	
+	public ReportController(ReportService reportService, ReportMapper reportMapper) {
+		this.reportService = reportService;
+	}
+	
+	
 	@GetMapping("/reportList")
 	public String reportList(Model model) {
 		
@@ -19,8 +36,11 @@ public class ReportController {
 	@GetMapping("/reportStandardList")
 	public String reportStandardList(Model model) {
 		
+		List<ReportStandard> reportStandardList = reportService.getReportStandardList();
+		
 		model.addAttribute("leftMenuList", "신고");
-		model.addAttribute("title", "신고 기준");
+		model.addAttribute("title", "신고 기준 목록 조회");
+		model.addAttribute("reportStandardList", reportStandardList);
 		
 		return "report/reportStandardList";
 		
