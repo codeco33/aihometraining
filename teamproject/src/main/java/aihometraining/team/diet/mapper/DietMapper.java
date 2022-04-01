675f3b1a6df64550c0f2134469b4f1c0257dadac
pokou125 <94342364+pokou125@users.noreply.github.com>
2022-04-02 02:34:43 +0900

    [LYH] 식단 은행관련 다수 업데이트

--- teamproject/src/main/java/aihometraining/team/diet/mapper/DietMapper.java
+++ teamproject/src/main/java/aihometraining/team/diet/mapper/DietMapper.java
@@ -1,16 +1,39 @@
 package aihometraining.team.diet.mapper;
 
 import java.util.List;
-import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
-import aihometraining.team.dto.ChallengeCategory;
+import aihometraining.team.dto.DietBank;
+import aihometraining.team.dto.DietMealLevelList;
+import aihometraining.team.dto.DietNutrientList;
 
 
 
 @Mapper
 public interface DietMapper {
-	//카테고리 목록 조회
+	//식단 은행 생성
+	public int insertDietBankList2(String connectEClass, String insertEmail,String newCode);
 
+	//식단 은행 새 코드 불러오기
+	public String selectDietBankListNewCode();
+	
+	//식단 은행 리스트 불러오기
+	public List<DietBank> getDietBankListAdmin();
+
+	//식단 은행 리스트 코드로 불러오기
+	public DietBank getDietBankListAdminByC(String newCode);
+
+	//식단 은행 리스트 삭제
+	public int deleteDietBankList(String dietBankCode);
+	
+	//음식 검색 대분류
+	public List<DietMealLevelList> selectDietMealLevelList();
+	
+	//음식 검색 소분류
+	public List<DietMealLevelList> selectDietMealListDetail(String mainMealSort);
+	
+	//음식 리스트 검색
+	public List<DietNutrientList> selectDietNutrientList(String mainMealSort);
+	
 }
