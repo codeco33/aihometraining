574ed59ac67c1b053975bef2f1a0559c04ab8c8f
ddoggang12 <ddoggang12@naver.com>
2022-04-12 00:52:04 +0900

    [CMH] dto 변경

--- teamproject/src/main/java/aihometraining/team/dto/WorkoutLog.java
+++ teamproject/src/main/java/aihometraining/team/dto/WorkoutLog.java
@@ -21,7 +21,7 @@ public class WorkoutLog {
 	 * workoutLogDeleteEmail
 	 */
 
-	private String workoutLogcode;
+	private String workoutLogCode;
 	private String memberEmail;
 	private String workoutLogLikeCount;
 	private String eClassCategorySmallCode;
@@ -40,14 +40,13 @@ public class WorkoutLog {
 	private List<FileDto> fileList;
 
 
-
-	public String getWorkoutLogcode() {
-		return workoutLogcode;
+	public String getWorkoutLogCode() {
+		return workoutLogCode;
 	}
 
 
-	public void setWorkoutLogcode(String workoutLogcode) {
-		this.workoutLogcode = workoutLogcode;
+	public void setWorkoutLogCode(String workoutLogCode) {
+		this.workoutLogCode = workoutLogCode;
 	}
 
 
@@ -193,7 +192,7 @@ public class WorkoutLog {
 
 	@Override
 	public String toString() {
-		return "WorkoutLog [workoutLogcode=" + workoutLogcode + ", memberEmail=" + memberEmail
+		return "WorkoutLog [workoutLogCode=" + workoutLogCode + ", memberEmail=" + memberEmail
 				+ ", workoutLogLikeCount=" + workoutLogLikeCount + ", eClassCategorySmallCode="
 				+ eClassCategorySmallCode + ", eClassCategorySmallName=" + eClassCategorySmallName
 				+ ", workoutLogTitle=" + workoutLogTitle + ", workoutLogContent=" + workoutLogContent
@@ -203,6 +202,10 @@ public class WorkoutLog {
 				+ ", workoutLogUpdateFinalDate=" + workoutLogUpdateFinalDate + ", workoutLogDeleteEmail="
 				+ workoutLogDeleteEmail + ", fileList=" + fileList + "]";
 	}
+
+		
+	
+	
 
 
 	
