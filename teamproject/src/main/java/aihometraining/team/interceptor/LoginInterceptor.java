4be192fe961883a2017e02161ad8e0f7bbe776bd
dldl8819 <dldl8819@naver.com>
2022-04-12 11:20:04 +0900

    [LMS] 회원 권한에 따라 페이지 경로 막기 작업
    DETAIL : 220412
    미인증 회원일 경우 /mypage, /admin 접근 불가하도록 작업
    인증한 일반 회원일 경우 /admin 접근 불가하도록 작업

--- teamproject/src/main/java/aihometraining/team/interceptor/LoginInterceptor.java
+++ teamproject/src/main/java/aihometraining/team/interceptor/LoginInterceptor.java
@@ -1,5 +1,8 @@
 package aihometraining.team.interceptor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -15,12 +18,65 @@ public class LoginInterceptor implements HandlerInterceptor{
 			throws Exception {
 		
 		HttpSession session = request.getSession();
+		String requestURI = request.getRequestURI();
 		
 		String sessionEmail 	= (String) session.getAttribute("SEMAIL");
 		
+		//인증한 일반 회원은 여기 못 들어간다.
+		List<String> authUser = new ArrayList<String>();
+		authUser.add("/admin/memberList");
+		authUser.add("/admin/memberLevelList");
+		authUser.add("/admin/loginHistory");
+		authUser.add("/주소");
+		authUser.add("/주소");
+		authUser.add("/주소");
+		authUser.add("/주소");
+		
+		//미인증 회원은 여기 못 들어간다.
+		List<String> user = new ArrayList<String>();
+		user.add("/admin/memberList");
+		user.add("/mypage");
+		user.add("/주소");
+		user.add("/주소");
+		user.add("/주소");
+		user.add("/주소");
+		user.add("/주소");
+		
+		// 로그인 안했을 시
 		if(sessionEmail == null) {
-			response.sendRedirect("/admin/login");
+			response.sendRedirect("/login");
 			return false;
+		}else {
+			String sessionLevelCode = (String) session.getAttribute("SLEVEL");
+			
+			if(sessionLevelCode != null && "levelCode002".equals(sessionLevelCode)) {
+				if(authUser.contains(requestURI)) {
+					response.sendRedirect("/");
+					return false;
+				}
+				
+				if(requestURI.indexOf("/admin") > -1) {					
+					response.sendRedirect("/");
+					return false;
+				}
+				
+			}else if(sessionLevelCode != null && "levelCode003".equals(sessionLevelCode)) {
+				if(user.contains(requestURI)) {
+					response.sendRedirect("/");
+					return false;
+				}
+				
+				
+				if(requestURI.indexOf("/admin") > -1) {					
+					response.sendRedirect("/");
+					return false;
+				}
+				if(requestURI.indexOf("/mypage") > -1) {					
+					response.sendRedirect("/");
+					return false;
+				}
+				
+			}
 		}
 		
 		return true;
