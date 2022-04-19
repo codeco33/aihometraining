14958828ed4b0889e23760c554f54dc257743c9a
jinsugyeong <apr15th@naver.com>
2022-04-19 18:02:36 +0900

    [team] eclassapproved DTO 수정

--- teamproject/src/main/java/aihometraining/team/dto/EClassApproved.java
+++ teamproject/src/main/java/aihometraining/team/dto/EClassApproved.java
@@ -2,10 +2,10 @@ package aihometraining.team.dto;
 
 
 public class EClassApproved {
-	
 	private String eClassApprovedCode;
-	private Member member;
 	private String memberEmail;
+	private String eClassCategorySmallCode;
+	private String eClassIntroduceCode;
 	private String eClassSectionTitleCode;
 	private String eClassSectionCurriculumCode;
 	private String eClassQuestionCode;
@@ -13,29 +13,42 @@ public class EClassApproved {
 	private String eClassApprovedPrice;
 	private String eClassApprovedName;
 	private int eClassApprovedSetDate;
+	private String eClassApprovedStratDate;
+	private String eClassApprovedEndDate;
+	private int eClassApprovedStudent;
 	private int eClassApprovedMaxStudent;
+	private String eClassApprovedDate;
+	private int eClassApprovedState;
+	private EClassCategorySmall eClassCategorySmall;
 	private EClassIntroduce eClassIntroduce;
 	private EClassSectionTitle eClassSectionTitle;
 	private EClassSectionCurriculum eClassSectionCurriculum;
 	private EClassQuestion eClassQuestion;
 	private EClassAnswer eClassAnswer;
+	private Member member;
 	public String geteClassApprovedCode() {
 		return eClassApprovedCode;
 	}
 	public void seteClassApprovedCode(String eClassApprovedCode) {
 		this.eClassApprovedCode = eClassApprovedCode;
 	}
-	public Member getMember() {
-		return member;
-	}
-	public void setMember(Member member) {
-		this.member = member;
-	}
 	public String getMemberEmail() {
 		return memberEmail;
 	}
 	public void setMemberEmail(String memberEmail) {
 		this.memberEmail = memberEmail;
+	}
+	public String geteClassCategorySmallCode() {
+		return eClassCategorySmallCode;
+	}
+	public void seteClassCategorySmallCode(String eClassCategorySmallCode) {
+		this.eClassCategorySmallCode = eClassCategorySmallCode;
+	}
+	public String geteClassIntroduceCode() {
+		return eClassIntroduceCode;
+	}
+	public void seteClassIntroduceCode(String eClassIntroduceCode) {
+		this.eClassIntroduceCode = eClassIntroduceCode;
 	}
 	public String geteClassSectionTitleCode() {
 		return eClassSectionTitleCode;
@@ -79,11 +92,47 @@ public class EClassApproved {
 	public void seteClassApprovedSetDate(int eClassApprovedSetDate) {
 		this.eClassApprovedSetDate = eClassApprovedSetDate;
 	}
+	public String geteClassApprovedStratDate() {
+		return eClassApprovedStratDate;
+	}
+	public void seteClassApprovedStratDate(String eClassApprovedStratDate) {
+		this.eClassApprovedStratDate = eClassApprovedStratDate;
+	}
+	public String geteClassApprovedEndDate() {
+		return eClassApprovedEndDate;
+	}
+	public void seteClassApprovedEndDate(String eClassApprovedEndDate) {
+		this.eClassApprovedEndDate = eClassApprovedEndDate;
+	}
+	public int geteClassApprovedStudent() {
+		return eClassApprovedStudent;
+	}
+	public void seteClassApprovedStudent(int eClassApprovedStudent) {
+		this.eClassApprovedStudent = eClassApprovedStudent;
+	}
 	public int geteClassApprovedMaxStudent() {
 		return eClassApprovedMaxStudent;
 	}
 	public void seteClassApprovedMaxStudent(int eClassApprovedMaxStudent) {
 		this.eClassApprovedMaxStudent = eClassApprovedMaxStudent;
+	}
+	public String geteClassApprovedDate() {
+		return eClassApprovedDate;
+	}
+	public void seteClassApprovedDate(String eClassApprovedDate) {
+		this.eClassApprovedDate = eClassApprovedDate;
+	}
+	public int geteClassApprovedState() {
+		return eClassApprovedState;
+	}
+	public void seteClassApprovedState(int eClassApprovedState) {
+		this.eClassApprovedState = eClassApprovedState;
+	}
+	public EClassCategorySmall geteClassCategorySmall() {
+		return eClassCategorySmall;
+	}
+	public void seteClassCategorySmall(EClassCategorySmall eClassCategorySmall) {
+		this.eClassCategorySmall = eClassCategorySmall;
 	}
 	public EClassIntroduce geteClassIntroduce() {
 		return eClassIntroduce;
@@ -115,15 +164,23 @@ public class EClassApproved {
 	public void seteClassAnswer(EClassAnswer eClassAnswer) {
 		this.eClassAnswer = eClassAnswer;
 	}
+	public Member getMember() {
+		return member;
+	}
+	public void setMember(Member member) {
+		this.member = member;
+	}
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
 		builder.append("EClassApproved [eClassApprovedCode=");
 		builder.append(eClassApprovedCode);
-		builder.append(", member=");
-		builder.append(member);
 		builder.append(", memberEmail=");
 		builder.append(memberEmail);
+		builder.append(", eClassCategorySmallCode=");
+		builder.append(eClassCategorySmallCode);
+		builder.append(", eClassIntroduceCode=");
+		builder.append(eClassIntroduceCode);
 		builder.append(", eClassSectionTitleCode=");
 		builder.append(eClassSectionTitleCode);
 		builder.append(", eClassSectionCurriculumCode=");
@@ -138,8 +195,20 @@ public class EClassApproved {
 		builder.append(eClassApprovedName);
 		builder.append(", eClassApprovedSetDate=");
 		builder.append(eClassApprovedSetDate);
+		builder.append(", eClassApprovedStratDate=");
+		builder.append(eClassApprovedStratDate);
+		builder.append(", eClassApprovedEndDate=");
+		builder.append(eClassApprovedEndDate);
+		builder.append(", eClassApprovedStudent=");
+		builder.append(eClassApprovedStudent);
 		builder.append(", eClassApprovedMaxStudent=");
 		builder.append(eClassApprovedMaxStudent);
+		builder.append(", eClassApprovedDate=");
+		builder.append(eClassApprovedDate);
+		builder.append(", eClassApprovedState=");
+		builder.append(eClassApprovedState);
+		builder.append(", eClassCategorySmall=");
+		builder.append(eClassCategorySmall);
 		builder.append(", eClassIntroduce=");
 		builder.append(eClassIntroduce);
 		builder.append(", eClassSectionTitle=");
@@ -150,8 +219,11 @@ public class EClassApproved {
 		builder.append(eClassQuestion);
 		builder.append(", eClassAnswer=");
 		builder.append(eClassAnswer);
+		builder.append(", member=");
+		builder.append(member);
 		builder.append("]");
 		return builder.toString();
 	}
-
+	
+	
 }
