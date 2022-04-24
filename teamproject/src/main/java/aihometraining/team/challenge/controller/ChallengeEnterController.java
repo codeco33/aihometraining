d29cfe41b25a49e9fce246cfb03bf067270816d2
codeco33 <codeco33@naver.com>
2022-04-24 17:14:55 +0900

    [CBR]
    20220424
    참가신청 처리 전 결제 화면으로 이동 후 결제시 참가 신청과 실행에 insert처리

--- teamproject/src/main/java/aihometraining/team/challenge/controller/ChallengeEnterController.java
+++ teamproject/src/main/java/aihometraining/team/challenge/controller/ChallengeEnterController.java
@@ -1,5 +1,6 @@
 package aihometraining.team.challenge.controller;
 
+import java.util.HashMap;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -15,9 +16,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import aihometraining.team.challenge.mapper.ChallengeEnterMapper;
 import aihometraining.team.challenge.service.ChallengeEnterService;
 import aihometraining.team.dto.ChallengeEnter;
 import aihometraining.team.dto.ChallengeGather;
+import aihometraining.team.dto.ChallengeGatherPlan;
 import aihometraining.team.dto.ChallengeGatherPlanDo;
 
 @Controller
@@ -34,6 +37,7 @@ private static final Logger log = LoggerFactory.getLogger(ChallengeEnterControll
 		this.challengeEnterService = challengeEnterService;
 	}
 	
+	//참가챌린지 목록 조회
 	@GetMapping("/challengeEnterList")
 	public String challengeEnterList(Model model, HttpSession session) {
 		
@@ -53,6 +57,21 @@ private static final Logger log = LoggerFactory.getLogger(ChallengeEnterControll
 		
 	}
 	
+	//결제후 참가처리
+	@PostMapping("/challengeEnterList")
+	public String challengeEnterInsert(ChallengeEnter challengeEnter,ChallengeGatherPlanDo challengeGatherPlanDo, HttpSession session) {
+		
+		log.info("참가챌린지 목록조회시 session: {}", session);
+		String sEmail = (String) session.getAttribute("SEMAIL");
+		
+		challengeEnterService.challengeEnterInsert(challengeEnter,challengeGatherPlanDo, sEmail);
+		
+		
+		return "redirect:/challenge/challengeEnter/challengeEnterList";
+		
+	}
+	
+	//참가챌린지 세부 조회
 	@GetMapping("/challengeEnterDetail")
 	public String challengeEnterDetail(Model model
 									  ,@RequestParam(value="challengeGatherCode", required = false) String challengeGatherCode) {
@@ -70,25 +89,27 @@ private static final Logger log = LoggerFactory.getLogger(ChallengeEnterControll
 		return "challenge/challengeEnter/challengeEnterDetail";
 		
 	}
+	
 	//챌린지 인증 화면
 	@GetMapping("/challengePlanDoInsert")
 	public String challengePlanDoInsert(Model model, 
-										@RequestParam(value="challengeEnterCode", required = false) String challengeEnterCode) {
+										@RequestParam(value="challengeGatherPlanDoCode", required = false) String challengeGatherPlanDoCode) {
 		
-		log.info("참가 챌린지  challengeEnterCode: {}", challengeEnterCode);
+		log.info("참가 챌린지  challengeGatherPlanDoCode: {}", challengeGatherPlanDoCode);
 		
-		List<ChallengeEnter> challengeEnterByCode = challengeEnterService.getChallengeEnterListByCode(challengeEnterCode);
+		//실행 코드별 챌린지 정보 조회
+		List<ChallengeGatherPlanDo> planDoProveByCode = challengeEnterService.getPlanDoProveByCode(challengeGatherPlanDoCode);
 		
-		log.info("참가 챌린지 정보조회  challengeEnterByCode: {}", challengeEnterByCode);
+		log.info("실행 코드별 챌린지 정보 조회  planDoProveByCode: {}", planDoProveByCode);
 		
 		model.addAttribute("title", "참가 챌린지 인증하기");
 		model.addAttribute("headerList", "챌린지");
-		model.addAttribute("challengeEnterByCode", challengeEnterByCode);
+		model.addAttribute("planDoProveByCode", planDoProveByCode);
 		
 		return "challenge/challengeEnter/challengePlanDoInsert";
 		
 	}
-	//챌린지 인증 처리
+	//참가 챌린지 인증 등록처리(update처리)
 	@PostMapping("/challengePlanDoInsert")
 	public String challengePlanDoInsert(ChallengeGatherPlanDo challengeGatherPlanDo
 										, HttpSession session
@@ -112,35 +133,63 @@ private static final Logger log = LoggerFactory.getLogger(ChallengeEnterControll
 		challengeEnterService.challengePlanDoInsert(challengeGatherPlanDo, sEmail, fileImage, fileRealPath);
 		
 		
-		return "redirect:/challenge/challengeEnter/challengePlanDoList";
+		return "redirect:/challenge/challengeEnter/challengeEnterList";
 		
 	}
 	
+	//챌린지 인증 리스트
 	@GetMapping("/challengePlanDoList")
-	public String challengePlanDoList(Model model) {
+	public String challengePlanDoList(Model model
+									 ,HttpSession session
+									 ,@RequestParam(value="challengeEnterCode", required = false) String challengeEnterCode) {
+		
+		String sEmail = (String) session.getAttribute("SEMAIL");
+		log.info("sEmail : {}", sEmail);
+		List<ChallengeGatherPlanDo> planDoList = challengeEnterService.getPlanDoListByCodeAndEmail(challengeEnterCode, sEmail);
 		
 		model.addAttribute("title", "인증 챌린지 목록");
 		model.addAttribute("leftMenuList", "챌린지");
+		model.addAttribute("planDoList", planDoList);
 		
 		return "challenge/challengeEnter/challengePlanDoList";
 	}
 	
-	@GetMapping("/challengeEnterPaymemt")
-	public String challengeEnterPaymemt(Model model) {
+	/*
+	 * @GetMapping("/challengeEnterPaymemt") public String
+	 * challengeEnterPaymemt(Model model) {
+	 * 
+	 * model.addAttribute("title", "챌린지 참가결제"); model.addAttribute("leftMenuList",
+	 * "챌린지");
+	 * 
+	 * return "challenge/challengeEnter/challengeEnterPaymemt";
+	 * 
+	 * }
+	 */
+	
+	@PostMapping("/challengeEnterPaymemt")
+	public String challengeEnterPaymemt(ChallengeGather challengeGather, Model model) {
 		
 		model.addAttribute("title", "챌린지 참가결제");
 		model.addAttribute("leftMenuList", "챌린지");
 		
+		log.info("20220422 challengeEnterPaymemt challengeGather값 조회  challengeGather : {}", challengeGather);
+		
+		/* DTO안에 DTO의 property값 받는 또 다른 방법
+		 * log.
+		 * info("20220422-2 challengeEnterPaymemt challengeGatherPlan값 조회  challengeGatherPlan : {}"
+		 * , challengeGatherPlan);
+		 */
+		
+		
+		log.info("돈내놔 : {}",challengeGather.getChallengeEnterDeposit());
+		
+		
+		
+		model.addAttribute("challengeGather", challengeGather);
+		/* DTO안에 DTO의 property값 받는 또 다른 방법
+		 * model.addAttribute("challengeGatherPlan", challengeGatherPlan); */
+		
 		return "challenge/challengeEnter/challengeEnterPaymemt";
-		
-	}
-	
-	@PostMapping("/challengeEnterPaymemt")
-	public String challengeEnterPaymemt(ChallengeGather challengeGather) {
-		
-		log.info("challengeGather 조회  challengeGather : {}", challengeGather);
-		
-		return "redirect:/challenge/challengeEnter/challengeEnterPaymemt";
 		
 	}
 }
