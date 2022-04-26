e57cf4575bf32fbea66299ca8cdd90a6d2a80598
jinsugyeong <apr15th@naver.com>
2022-04-26 17:11:57 +0900

    [JSG] paymentDetail 관리자 페이지 오류 수정

--- teamproject/src/main/java/aihometraining/team/controller/PaymentAdminController.java
+++ teamproject/src/main/java/aihometraining/team/controller/PaymentAdminController.java
@@ -6,17 +6,36 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import aihometraining.team.challenge.mapper.ChallengeGatherMapper;
+import aihometraining.team.mapper.MemberMapper;
+import aihometraining.team.mapper.PaymentMapper;
+import aihometraining.team.service.MemberService;
+import aihometraining.team.service.PaymentService;
 
 
 @Controller
 @RequestMapping("/admin")
 public class PaymentAdminController {
 	
+	//DI 의존성 주입 생성자 메소드 주입 방식
+		private PaymentMapper paymentMapper;
+		private PaymentService paymentService;
+		private ChallengeGatherMapper challengeGatherMapper; 
+		
+		public PaymentAdminController(PaymentMapper paymentMapper, PaymentService paymentService
+									,ChallengeGatherMapper challengeGatherMapper) {
+			this.paymentMapper = paymentMapper;
+			this.paymentService = paymentService;
+			this.challengeGatherMapper = challengeGatherMapper;
+		}
 	
+	/** 
+	 * 관리자 결제 내역 조회
+	 * */
 	@GetMapping("/paymentList")
 	public String paymentList(Model model) {
-		
-		
 		
 		model.addAttribute("leftMenuList", "결제/환불");
 		model.addAttribute("title", "결제 내역");
@@ -24,8 +43,14 @@ public class PaymentAdminController {
 		return "payment/paymentList";
 	}
 	
+	/**
+	 * 관리자 결제 상세 내역 조회
+	 */
 	@GetMapping("/paymentList/paymentDetail")
-	public String paymentDetail(Model model) {
+	public String paymentDetail(Model model,@RequestParam(name="paymentCode") String paymentCode) {
+		
+		MypageController mc = new MypageController(null, null, paymentMapper, paymentService, challengeGatherMapper);
+		mc.paymentDetail(model, paymentCode);
 		
 		
 		model.addAttribute("leftMenuList", "결제/환불");
